1163a9414eaca8bff0c63bb800ed84055515fb04
Meowch <1311787255@qq.com>
2021-01-23 18:11:40 +0800

    replace ResNet152 with ResNet50

--- ResNet50.py
+++ ResNet50.py
@@ -9,15 +9,21 @@ class Net(nn.Module):
         # 可以选择冻结卷积层
         # for p in self.parameters():
         #     p.requires_grad = False
-        self.fc = nn.Linear(in_features=2048, out_features=102)
+        # self.fc = nn.Linear(in_features=2048, out_features=102)
+        self.classifier = nn.Sequential(
+            nn.Linear(2048, 512, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.8, inplace=False),
+            nn.Linear(512, 102, bias=True)
+        )
 
     def forward(self, x):
         x = self.resnet(x)
         x = x.view(x.shape[0], -1)
-        x = self.fc(x)
+        x = self.classifier(x)
         return x
 
 
-resnet152 = models.resnet152(pretrained=True)
-net = Net(resnet152)
+resnet50 = models.resnet50(pretrained=True)
+net = Net(resnet50)
 
